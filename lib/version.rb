5941fded30c4bf95df891edaf4dcfbf6450594c7
Alexis Vanier <alexis.vanier@samsung.com>
2018-09-21 15:29:33 -0400

    v0.1.6

--- lib/version.rb
+++ lib/version.rb
@@ -6,7 +6,7 @@ module AdGear
     module GroupManager
       module Version
         # The global constant holding the version of the gem.
-        GEM_VERSION = '0.1.5'.freeze
+        GEM_VERSION = '0.1.6'.freeze
       end
     end
   end
