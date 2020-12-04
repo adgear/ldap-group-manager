d83bd14ed16f4307983305bf2a547073aa9747bd
Alexis Vanier <alexis.vanier@samsung.com>
2020-12-04 15:17:21 -0500

    Fix Gemfile for 1.3.1 release and bump to 1.3.1

--- lib/version.rb
+++ lib/version.rb
@@ -6,7 +6,7 @@ module AdGear
     module GroupManager
       module Version
         # The global constant holding the version of the gem.
-        GEM_VERSION = '1.3.0'.freeze
+        GEM_VERSION = '1.3.1'.freeze
       end
     end
   end
